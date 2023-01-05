219404613f91a797ef313a993fc67fd7136e3423
hyeonjeong Park <py0429@ewhain.net>
2023-01-05 19:47:06 +0900

    #6 [Feat] controller 분기 처리

--- src/controller/recommendController.ts
+++ src/controller/recommendController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
 import { fail, success } from "../constants/response";
 import { rm, sc } from "../constants";
+import { recommendService } from "../service";
 
 const getRecommend = async (req: Request, res: Response) => {
+    const data = await recommendService.getRecommend();
 
+    if (!data) {
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.FAIL_GET_RECOMMEND));
+    }
+
+    return res.status(sc.OK).send(success(sc.OK, rm.SUCCESS_GET_RECOMMEND, data));
 }
 
 const recommendController = {
