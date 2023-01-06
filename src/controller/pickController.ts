1c9b0a8859f6a15ef7ce6ae9624b6521eca7c358
hyeonjeong Park <py0429@ewhain.net>
2023-01-07 02:52:20 +0900

    #27 [Feat] Pick service 구현

--- src/controller/pickController.ts
+++ src/controller/pickController.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from "express";
 import { fail, success } from "../constants/response";
 import { rm, sc } from "../constants";
+import { pickService } from "../service";
+import { PickPatchRequestDTO } from "../interfaces/pick/PickPatchRequestDTO";
 
-const pickRouter = async (req: Request, res: Response) => {
-    const { previousPick, newPick } = req.body;
+//* Pick한 책 수정
+const patchPick = async (req: Request, res: Response) => {
+    const pickPatchRequestDTO: PickPatchRequestDTO = req.body;
 
-    const data = await pickService.patchPick();
+
+    const data = await pickService.patchPick(pickPatchRequestDTO);
+
+    if (!data) {
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.FAIL_PATCH_PICK));
+    }
+
+    return res.status(sc.OK).send(success(sc.OK, rm.SUCCESS_PATCH_PICK, data));
 
 }
 
 const pickController = {
-    pickRouter,
+    patchPick,
 }
 
 export default pickController;
--- src/service/pickService.ts
+++ src/service/pickService.ts
@@ -1,8 +1,54 @@
 import { PrismaClient } from "@prisma/client";
-
+import { PickPatchRequestDTO } from "../interfaces/pick/PickPatchRequestDTO";
 const prisma = new PrismaClient();
 
-const patchPick = async () => {
+//* Pick한 책 수정
+const patchPick = async (pickPatchRequestDTO: PickPatchRequestDTO) => {
+
+    // 원래 pick값 false로 수정하기
+    const previousPick = await prisma.bookshelf.updateMany({
+        where: {
+            pickIndex: { in: [1, 2, 3] },
+        },
+        data: {
+            pickIndex: undefined,
+        }
+    });
+
+    // 새롭게 pick한 값 index 세팅하기
+    const firstPick = await prisma.bookshelf.updateMany({
+        where: {
+            bookId: pickPatchRequestDTO.firstPick,
+        },
+        data: {
+            pickIndex: 1
+        }
+    });
+
+    const secondPick = await prisma.bookshelf.updateMany({
+        where: {
+            bookId: pickPatchRequestDTO.secondPick,
+        },
+        data: {
+            pickIndex: 2,
+        }
+    })
+
+    const thirdPick = await prisma.bookshelf.updateMany({
+        where: {
+            bookId: pickPatchRequestDTO.thirdPick,
+        },
+        data: {
+            pickIndex: 3,
+        }
+    })
+
+    let data: { first: object, second: object, third: object } = {
+        first: firstPick,
+        second: secondPick,
+        third: thirdPick
+    }
+    return data;
 
 }
 
