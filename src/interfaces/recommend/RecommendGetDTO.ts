f68d80e0fb3ec11f94f13fdbbe9ef03d23a18c8e
hyeonjeong Park <py0429@ewhain.net>
2023-01-08 02:45:30 +0900

    #41 [Fix] recommendGetDto 생성

---
+++ src/interfaces/recommend/RecommendGetDTO.ts
@@ -0,0 +1,11 @@
+export interface RecommendGetDTO {
+    recommendId: number,
+    createdAt: string,
+    recommendDesc?: string,
+    friendId: string,
+    bookId: string,
+    bookTitle: string,
+    author: string,
+    bookImage: string,
+
+}
