c0e23f6f12be7f943a785c03c2b671076d776783
hyeonjeong Park <py0429@ewhain.net>
2023-01-07 23:52:32 +0900

    #30 [Feat] alarm service

--- src/router/alarmRouter.ts
+++ src/router/alarmRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { alarmController } from "../controller";
 
 const router: Router = Router();
 
 //* 알림 조회 GET - /alarm
-router.get("/")
+router.get("/", alarmController.getAlarm);
 
 export default router;
--- src/service/alarmService.ts
+++ src/service/alarmService.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
+import { AlarmResponseDTO } from "../interfaces/alarm/AlarmResponseDTO";
 
 const prisma = new PrismaClient();
 
@@ -9,11 +11,42 @@ const getAlarm = async () => {
         where: {
             receiverId: 1,
         },
-    })
+        select: {
+            typeId: true,
+            senderId: true,
+            createdAt: true,
+        }
+    });
 
-    const promises = 
+    let alarmAll: AlarmResponseDTO[] = [];
 
-    
+    const alarmDetailData = await Promise.all(
+        alarmData.map((data: any) => {
+
+            // 팔로우 한 경우
+            if (data.typeId === 1) {
+                const followResult = {
+                    typeId: data.typeId,
+                    senderId: data.senderId,
+                    createdAt: dayjs(data.createdAt).format('YYYY-MM-DD')
+                };
+                alarmAll.push(followResult);
+
+            }
+            // 친구 책 추천, 책 추가한 경우
+            else {
+                const bookResult = {
+                    typeId: data.typeId,
+                    senderId: data.senderId,
+                    createdAt: dayjs(data.createdAt).format('YYYY-MM-DD')
+                }
+                alarmAll.push(bookResult);
+            }
+
+        })
+    );
+
+    return alarmAll;
 }
 
 const alarmService = {
