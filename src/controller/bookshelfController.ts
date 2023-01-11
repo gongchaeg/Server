f125114e53f63073db4840fd58165181ce27c721
yeseul106 <20191037@sungshin.ac.kr>
2023-01-11 23:45:07 +0900

    #90 [Add] request header 누락 시 에러 처리 코드

--- src/controller/bookshelfController.ts
+++ src/controller/bookshelfController.ts
@@ -54,6 +54,11 @@ const getBookById = async (req: Request, res: Response) => {
     const auth = req.header("auth");
     const { bookId } = req.params;
 
+    //* 헤더로 유저 아이디 안넘겨줬을 때
+    if (!auth) {
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+    }
+
     try {
         if (!bookId) {
             return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NOT_FOUND));       
