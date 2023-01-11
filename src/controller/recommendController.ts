bc770168af289b566b86c9061684fb70b733a1f8
hyeonjeong Park <py0429@ewhain.net>
2023-01-11 19:59:38 +0900

    #87 [add] request header 추가

--- src/controller/recommendController.ts
+++ src/controller/recommendController.ts
@@ -7,7 +7,6 @@ import { recommendService } from "../service";
 const getRecommend = async (req: Request, res: Response) => {
     const data = await recommendService.getRecommend();
     const auth = req.header("auth");
-    console.log(auth);
 
     if (!data) {
         return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.FAIL_GET_RECOMMEND));
