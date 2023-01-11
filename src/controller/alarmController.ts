301354f3ac0ef3b317d2530482cddc6ed3f325f2
hyeonjeong Park <py0429@ewhain.net>
2023-01-11 22:54:22 +0900

    #87 [add] request header 추가

--- src/controller/alarmController.ts
+++ src/controller/alarmController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { rm, sc } from "../constants";
 import { fail, success } from "../constants/response";
 import { alarmService } from "../service";
+import { slackErrorMessage } from "../modules/slackErrorMessage";
+import { sendWebhookMessage } from "../modules/slackWebhook";
 
 const getAlarm = async (req: Request, res: Response) => {
     const auth = req.header("auth");
@@ -14,7 +16,12 @@ const getAlarm = async (req: Request, res: Response) => {
 
         return res.status(sc.OK).send(success(sc.OK, rm.SUCCESS_GET_ALARM, data));
     } catch (error) {
+        const errorMessage = slackErrorMessage(req.method.toUpperCase(), req.originalUrl, error, 1, req.statusCode);
 
+        sendWebhookMessage(errorMessage);
+
+        res.status(sc.INTERNAL_SERVER_ERROR)
+            .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
     }
 
 }
