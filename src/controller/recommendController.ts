0bb509beba783fd58472a43daebfb0e5ea637d52
hyeonjeong Park <py0429@ewhain.net>
2023-01-11 23:43:31 +0900

    #87 [Add] user headre 변수 변경

--- src/controller/recommendController.ts
+++ src/controller/recommendController.ts
@@ -2,17 +2,29 @@ import { Request, Response } from "express";
 import { fail, success } from "../constants/response";
 import { rm, sc } from "../constants";
 import { recommendService } from "../service";
+import { sendWebhookMessage } from './../modules/slackWebhook';
+import { slackErrorMessage } from "../modules/slackErrorMessage";
 
 //* 추천 책 조회하기
 const getRecommend = async (req: Request, res: Response) => {
     const data = await recommendService.getRecommend();
     const auth = req.header("auth");
 
-    if (!data) {
-        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.FAIL_GET_RECOMMEND));
+    try {
+        if (!data) {
+            return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.FAIL_GET_RECOMMEND));
+        }
+
+        return res.status(sc.OK).send(success(sc.OK, rm.SUCCESS_GET_RECOMMEND, data));
+    } catch (error) {
+        const errorMessage = slackErrorMessage(req.method.toUpperCase(), req.originalUrl, error, +{ auth }, req.statusCode);
+
+        sendWebhookMessage(errorMessage);
+
+        res.status(sc.INTERNAL_SERVER_ERROR)
+            .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
     }
 
-    return res.status(sc.OK).send(success(sc.OK, rm.SUCCESS_GET_RECOMMEND, data));
 }
 
 const recommendController = {
