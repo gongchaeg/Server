d746a53c476e7e30adbb1803ce9e8f4a490fea0d
yeseul106 <20191037@sungshin.ac.kr>
2023-01-12 21:15:57 +0900

    #103 [Test] 등록한 책 삭제 after 로직 추가

--- test/bookshelf.spec.ts
+++ test/bookshelf.spec.ts
@@ -10,9 +10,8 @@ dotenv.config();
 const prisma = new PrismaClient();
 
 describe('***** Bookshelf Test *****', () => {
-//* 내 책장에 책 등록 API
+    //* 내 책장에 책 등록 API
     context('[POST] /bookshelf', () => {
-
         after(async () => {
             const book = await prisma.book.findFirst({
                 where : {
@@ -88,102 +87,129 @@ describe('***** Bookshelf Test *****', () => {
         });
     });
         
-//* 등록한 책 상세 정보 조회
-context('[GET] /bookshelf/detail/3', () => {
-    it('등록한 책 상세 정보 조회 성공', done => { 
-        req(app)
-            .get('/bookshelf/detail/3')  // api 요청
-            .set('Content-Type', 'application/json')
-            .set('auth', '300')  // header - userId 설정
-            .expect(200) // 예측 상태 코드
-            .expect('Content-Type', /json/) // 예측 content-type
-            .then(res => {
-                expect(res.body.data.Book.bookTitle).to.equal("오리진");  
-                expect(res.body.data.Book.author).to.equal("댄브라운"); //response body 예측값 검증
-                done();
-            })
-            .catch(err => {
-                console.error("###### Error >>", err);
-                done(err);
-            })
+    //* 등록한 책 상세 정보 조회
+    context('[GET] /bookshelf/detail/3', () => {
+        it('등록한 책 상세 정보 조회 성공', done => { 
+            req(app)
+                .get('/bookshelf/detail/3')  // api 요청
+                .set('Content-Type', 'application/json')
+                .set('auth', '300')  // header - userId 설정
+                .expect(200) // 예측 상태 코드
+                .expect('Content-Type', /json/) // 예측 content-type
+                .then(res => {
+                    expect(res.body.data.Book.bookTitle).to.equal("오리진");  
+                    expect(res.body.data.Book.author).to.equal("댄브라운"); //response body 예측값 검증
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+        it('필요한 헤더 값이 없음', done => {
+            req(app)
+                .get('/bookshelf/detail/1')
+                .set('Content-Type', 'application/json')
+                .expect(400)
+                .then(res => {
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+        it('해당하는 책을 찾을 수 없음', done => {
+            req(app)
+                .get('/bookshelf/detail/200')
+                .set('Content-Type', 'application/json')
+                .then(res => {
+                    expect(res.body.status).to.equals(400); // 추후 상태 코드 변경
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
     });
-    it('필요한 헤더 값이 없음', done => {
-        req(app)
-            .get('/bookshelf/detail/1')
-            .set('Content-Type', 'application/json')
-            .expect(400)
-            .then(res => {
-                done();
-            })
-            .catch(err => {
-                console.error("###### Error >>", err);
-                done(err);
-            })
-    });
-    it('해당하는 책을 찾을 수 없음', done => {
-        req(app)
-            .get('/bookshelf/detail/200')
-            .set('Content-Type', 'application/json')
-            .then(res => {
-                expect(res.body.status).to.equals(400); // 추후 상태 코드 변경
-                done();
-            })
-            .catch(err => {
-                console.error("###### Error >>", err);
-                done(err);
-            })
-    });
-});
 
-//* 등록한 책 삭제
-// context('[DELETE] /bookshelf/1', () => {
-//     it('책 삭제하기 성공', done => {
-//         req(app)
-//             .delete('/bookshelf/1')  // api 요청
-//             .set('Content-Type', 'application/json')
-//             .set('auth', '300')  // header - userId 설정
-//             .expect(200) // 예측 상태 코드
-//             .expect('Content-Type', /json/) // 예측 content-type
-//             .then(res => {
-//                 done();
-//             })
-//             .catch(err => {
-//                 console.error("###### Error >>", err);
-//                 done(err);
-//             })
-//     });
-//     it('필요한 헤더 값이 없음', done => {
-//         req(app)
-//             .delete('/bookshelf/1')
-//             .set('Content-Type', 'application/json')
-//             .expect(400)
-//             .then(res => {
-//                 done();
-//             })
-//             .catch(err => {
-//                 console.error("###### Error >>", err);
-//                 done(err);
-//             })
-//     });
-//     // it('해당하는 책을 찾을 수 없음', done => {
-//     //     req(app)
-//     //         .delete('/bookshelf/200')
-//     //         .set('Content-Type', 'application/json')
-//     //         .expect(404)
-//     //         .then(res => {
-//     //             done();
-//     //         })
-//     //         .catch(err => {
-//     //             console.error("###### Error >>", err);
-//     //             done(err);
-//     //         })
-//     // });
-// });
-    
-//* 등록한 책 수정
-    
-//* 내 책장 (메인 뷰) 조회
-    
-//* 친구 책장 조회
+    //* 등록한 책 삭제
+    context('[DELETE] /bookshelf/3', () => {
+        after(async () => {
+            await prisma.bookshelf.create({
+                data: {
+                pickIndex : 0,
+                description : "Test 책입니다.",
+                memo : "엇",
+                User : { 
+                    connect : {
+                    id : 300
+                    }
+                },
+                Book : {
+                    connectOrCreate : {
+                    where : {
+                        id : 3
+                    },
+                    create : {
+                        bookTitle : "오리진",
+                        author : "댄브라운",
+                        bookImage : "https://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_3239339%2F32393392566.jpg&type=w216"
+                    }
+                    }
+                }
+                }
+            });
+        });
+
+        it('책 삭제하기 성공', done => {
+            req(app)
+                .delete('/bookshelf/3')  // api 요청
+                .set('Content-Type', 'application/json')
+                .set('auth', '300')  // header - userId 설정
+                .expect(200) // 예측 상태 코드
+                .expect('Content-Type', /json/) // 예측 content-type
+                .then(res => {
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+        it('필요한 헤더 값이 없음', done => {
+            req(app)
+                .delete('/bookshelf/3')
+                .set('Content-Type', 'application/json')
+                .expect(400)
+                .then(res => {
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+        it('해당하는 책을 찾을 수 없음', done => {
+            req(app)
+                .delete('/bookshelf/200')
+                .set('Content-Type', 'application/json')
+                .expect(400) // 추후 상태 코드 변경
+                .then(res => {
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+    });
+        
+    //* 등록한 책 수정
+        
+    //* 내 책장 (메인 뷰) 조회
+        
+    //* 친구 책장 조회
 
 });
