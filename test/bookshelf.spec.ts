4e67ed222af5540a791d09133e25de749d949cb3
yeseul106 <20191037@sungshin.ac.kr>
2023-01-12 18:10:16 +0900

    #97 [Add] 테스트 동작 후 after 로직 추가

--- test/bookshelf.spec.ts
+++ test/bookshelf.spec.ts
@@ -2,54 +2,167 @@ import app from "../src/index";
 import req from "supertest";
 import expect from "chai";
 import dotenv from "dotenv";
+import { Response } from 'supertest';
+import { PrismaClient } from '@prisma/client';
+import { afterEach } from 'node:test';
 
 dotenv.config();
+const prisma = new PrismaClient();
 
-//* 내 책장에 책 등록 API
-describe('POST /bookshelf', () => {
-  it('내 책장에 책 등록하기 성공', done => {
-      req(app)
-          .post('/bookshelf')  // api 요청
-          .set('Content-Type', 'application/json')
-          .set('auth', '1')  // header - userId 설정
-          .send({
-                "bookImage" : "book image url",
-                "bookTitle" : "노력의 기쁨과 슬픔",
-                "author" : "올리비에 프리올",
-                "description" : "책에 대한 한 마디..",
-                "memo" : "책에 대한 메모.."
-          }) // request body
-          .expect(201) // 예측 상태 코드
-          .expect('Content-Type', /json/) // 예측 content-type
-          .then(res => {
-              done();
-          })
-          .catch(err => {
-              console.error("######Error >>", err);
-              done(err);
-          })
-  });
-  it('필요한 헤더 값이 없음', done => {
-      req(app)
-          .get('/bookshelf')
-          .set('Content-Type', 'application/json')
-          .expect(400)
-          .then(res => {
-              done();
-          })
-          .catch(err => {
-              console.error("######Error >>", err);
-              done(err);
-          })
-  });
+describe('***** Bookshelf Test *****', () => {
+    //* 내 책장에 책 등록 API
+    context('[POST] /bookshelf', () => {
+
+        after(async () => {
+            const book = await prisma.book.findFirst({
+                where : {
+                    bookTitle: "아무튼, 여름",
+                    author : "김신회"
+                }
+            });
+
+            await prisma.bookshelf.deleteMany({
+                where : {
+                userId : 300,
+                bookId : book?.id
+                }
+            })
+        });
+
+        it("내 책장에 책 등록 성공", done => {
+            req(app)
+            .post('/bookshelf')  // api 요청
+            .set('Content-Type', 'application/json')
+            .set('auth', '300')  // header - userId 설정
+            .send({
+                "bookImage" : "http://image.yes24.com/goods/90365124/XL",
+                "bookTitle" : "아무튼, 여름",
+                "author" : "김신회",
+                "description" : "테스트를 위한 한 마디에요",
+                "memo" : "테스트를 위한 메모에요"
+            }) // request body
+            .expect(201) // 예측 상태 코드
+            .expect('Content-Type', /json/) // 예측 content-type
+            .then(res => {
+                done();
+            })
+            .catch(err => {
+                console.error("###### Error >>", err);
+                done(err);
+            })
+        })
+
+        it('필요한 헤더 값이 없음', done => {
+            req(app)
+                .get('/bookshelf')
+                .set('Content-Type', 'application/json')
+                .expect(400)
+                .then(res => {
+                    done();
+                })
+                .catch(err => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+    });
+        
+    //* 등록한 책 상세 정보 조회
+//     context('[GET] /bookshelf/detail/1', () => {
+//         it('등록한 책 상세 정보 조회 성공', done => { 
+//             req(app)
+//                 .get('/bookshelf/detail/1')  // api 요청
+//                 .set('Content-Type', 'application/json')
+//                 .set('auth', '300')  // header - userId 설정
+//                 .expect(200) // 예측 상태 코드
+//                 .expect('Content-Type', /json/) // 예측 content-type
+//                 .then(res => {
+//                     expect(res.body.data.Book.bookTitle).to.equal("하나님의 숨결");  
+//                     expect(res.body.data.Book.author).to.equal("김병삼"); //response body 예측값 검증
+//                     done();
+//                 })
+//                 .catch(err => {
+//                     console.error("###### Error >>", err);
+//                     done(err);
+//                 })
+//         });
+//         it('필요한 헤더 값이 없음', done => {
+//             req(app)
+//                 .get('/bookshelf/detail/1')
+//                 .set('Content-Type', 'application/json')
+//                 .expect(400)
+//                 .then(res => {
+//                     done();
+//                 })
+//                 .catch(err => {
+//                     console.error("###### Error >>", err);
+//                     done(err);
+//                 })
+//         });
+//         // it('해당하는 책을 찾을 수 없음', done => {
+//         //     req(app)
+//         //         .get('/bookshelf/detail/200')
+//         //         .set('Content-Type', 'application/json')
+//         //         .then(res => {
+//         //             expect(res.body.status).to.equals(404);
+//         //             done();
+//         //         })
+//         //         .catch(err => {
+//         //             console.error("###### Error >>", err);
+//         //             done(err);
+//         //         })
+//         // });
+//     });
+    
+//     //* 등록한 책 삭제
+//     context('[DELETE] /bookshelf/1', () => {
+//         it('책 삭제하기 성공', done => {
+//             req(app)
+//                 .delete('/bookshelf/1')  // api 요청
+//                 .set('Content-Type', 'application/json')
+//                 .set('auth', '300')  // header - userId 설정
+//                 .expect(200) // 예측 상태 코드
+//                 .expect('Content-Type', /json/) // 예측 content-type
+//                 .then(res => {
+//                     done();
+//                 })
+//                 .catch(err => {
+//                     console.error("###### Error >>", err);
+//                     done(err);
+//                 })
+//         });
+//         it('필요한 헤더 값이 없음', done => {
+//             req(app)
+//                 .delete('/bookshelf/1')
+//                 .set('Content-Type', 'application/json')
+//                 .expect(400)
+//                 .then(res => {
+//                     done();
+//                 })
+//                 .catch(err => {
+//                     console.error("###### Error >>", err);
+//                     done(err);
+//                 })
+//         });
+//         // it('해당하는 책을 찾을 수 없음', done => {
+//         //     req(app)
+//         //         .delete('/bookshelf/200')
+//         //         .set('Content-Type', 'application/json')
+//         //         .expect(404)
+//         //         .then(res => {
+//         //             done();
+//         //         })
+//         //         .catch(err => {
+//         //             console.error("###### Error >>", err);
+//         //             done(err);
+//         //         })
+//         // });
+//     });
+    
+//     //* 등록한 책 수정
+    
+//     //* 내 책장 (메인 뷰) 조회
+    
+//     //* 친구 책장 조회
+
 });
-
-//* 등록한 책 상세 정보 조회
-
-//* 등록한 책 삭제
-
-//* 등록한 책 수정
-
-//* 내 책장 (메인 뷰) 조회
-
-//* 친구 책장 조회
---
+++ test/friend.spec.ts
@@ -0,0 +1,85 @@
+import app from "../src/index";
+import req from "supertest";
+import { expect } from "chai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+describe('***** Friend Test *****', () => {
+    context('[GET] /friend?nickname={}', () => {
+        it('사용자 검색하기 성공', done => {
+            req(app)
+                .get(encodeURI('/friend?nickname=예스리'))  // api 요청
+                .set('Content-Type', 'application/json')
+                .set('auth', '300')  // header 설정
+                .expect(200) // 예측 상태 코드
+                .expect('Content-Type', /json/) // 예측 content-type
+                .then((res) => {
+                    done();
+                })
+                .catch((err) => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+    });
+
+    context('[POST] /friend/:friendId', () => {
+        it('팔로우 하기 성공', done => {
+            req(app)
+                .post('/friend/301')
+                .set('Content-Type', 'application/json')
+                .set('auth', '300')
+                .expect(200)
+                .expect('Content-Type', /json/) // 예측 content-type
+                .then((res) => {
+                    done();
+                })
+                .catch((err) => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+        it('이미 팔로우 한 경우, 팔로우 하기 실패 ', done => {
+            req(app)
+                .post('/friend/4')
+                .set('Content-Type', 'application/json')
+                .set('auth', '300')
+                .expect(400)
+                .expect('Content-Type', /json/) // 예측 content-type
+                .then((res) => {
+                    done();
+                })
+                .catch((err) => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+    });
+
+    context('[POST] /friend/:friendId/recommend', () => {
+        it('친구에게 책 추천하기 성공', done => {
+            req(app)
+                .post('/friend/3/recommend')
+                .set('Content-Type', 'application/json')
+                .set('auth', '300')
+                .send({
+                    "recommendDesc": "좋아요 너무나도 ~~",
+                    "bookTitle": "잘될 수밖에 없는 너에게",
+                    "author": "최서영",
+                    "bookImage": "http://image.yes24.com/goods/111750543/XL"
+                })
+                .expect(200)
+                .expect('Content-Type', /json/) // 예측 content-type
+                .then((res) => {
+                    done();
+                })
+                .catch((err) => {
+                    console.error("###### Error >>", err);
+                    done(err);
+                })
+        });
+    })
+
+});
+
