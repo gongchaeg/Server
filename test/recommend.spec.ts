05048a5e8bf9aa6c428c2bf092a8dd418bd7a8c6
hyeonjeong Park <py0429@ewhain.net>
2023-01-12 16:06:17 +0900

    #109 [Fix] user 수정

--- test/recommend.spec.ts
+++ test/recommend.spec.ts
@@ -11,7 +11,7 @@ describe('***** Recommend Test *****', () => {
             req(app)
                 .get('/recommend')  // api 요청
                 .set('Content-Type', 'application/json')
-                .set('auth', '1')  // header 설정
+                .set('auth', '300')  // header 설정
                 .expect(200) // 예측 상태 코드
                 .expect('Content-Type', /json/) // 예측 content-type
                 .then((res) => {
