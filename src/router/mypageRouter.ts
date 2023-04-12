050ed5c1ecb14e5396b5ec6b84ef9c5d7598d4e6
hyeonjeong Park <py0429@ewhain.net>
2023-04-13 01:36:15 +0900

    #197-patch-user

--- src/router/mypageRouter.ts
+++ src/router/mypageRouter.ts
@@ -7,9 +7,9 @@ const router: Router = Router();
 //* 사용자 삭제하기 DELETE /mypage/withdraw
 router.delete("/withdraw", auth, mypageController.deleteUser);
 //* 프로필 수정하기 PATCH /mypage/profile
-router.patch("/profile", auth, upload.single("file"), mypageController.patchUser);
+router.patch("/profile", upload.single("file"), auth, mypageController.patchUser);
 //* 사용자 정보 조회하기 GET /mypage/profile
-router.get("/profile", auth, upload.single("file"), mypageController.getUserData);
+router.get("/profile", upload.single("file"), auth, mypageController.getUserData);
 //* 차단 리스트에서 차단 해제하기 DELETE /mypage/blocklist/:friendId
 router.delete("/blocklist/:friendId", auth, mypageController.cancleBlock);
 //* 차단 리스트 조회하기 GET /mypage/blocklist
