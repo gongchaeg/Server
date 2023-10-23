c73f692f61988a0cb7a6b1aa33c7dc31b98b3e7a
hyeonjeongs <py0429@ewhain.net>
2023-10-24 00:07:23 +0900

    #229 [feat] version add

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -7,6 +7,6 @@ const router: Router = Router();
 //* 닉네임 중복 검사 POST /user/duplicate
 router.post("/duplicate", userController.postDuplicateNickname);
 //* 유저 업데이트 버전 확인하기 GET /user/version
-router.get("/version", auth, userController.getUserVersion)
+router.get("/v1/version", auth, userController.getUserVersion)
 
 export default router;
