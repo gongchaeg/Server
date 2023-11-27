c63ace45a7da0e02a464e32676885e7a2319cc68
yeseul106 <20191037@sungshin.ac.kr>
2023-11-28 01:36:14 +0900

    [Fix] 버전 정보 내려줄 때, nullable 처리

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -2,105 +2,119 @@ import { PrismaClient } from "@prisma/client";
 import { IntroDTO } from "../interfaces/user/IntroDTO";
 import { UserDTO } from "../interfaces/user/UserDTO";
 import { UserDuplicateDTO } from "../interfaces/user/UserDuplicateDTO";
+import { UserVersionDTO } from "../interfaces/user/UserVersionDTO";
 
 const prisma = new PrismaClient();
 
 //* 사용자 조회
 const getUser = async (userId: number): Promise<UserDTO> => {
-    const userData = await prisma.user.findFirst({
-        where: {
-            id: userId
-        },
-        select: {
-            id: true,
-            nickname: true,
-            profileImage: true
-        }
-    });
-    if (!userData) throw new Error('no user!');
+  const userData = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      nickname: true,
+      profileImage: true,
+    },
+  });
+  if (!userData) throw new Error("no user!");
 
-    return userData;
-}
+  return userData;
+};
 
 //* 사용자 정보 조회
 const getUserIntro = async (userId: number) => {
-    const userIntro: IntroDTO | null = await prisma.user.findUnique({
-        where: {
-            id: userId
-        },
-        select: {
-            id: true,
-            nickname: true,
-            profileImage: true,
-            intro: true
-        }
-    });
-    if (!userIntro) throw new Error('no userIntro!');
+  const userIntro: IntroDTO | null = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      nickname: true,
+      profileImage: true,
+      intro: true,
+    },
+  });
+  if (!userIntro) throw new Error("no userIntro!");
 
-    return userIntro;
-}
+  return userIntro;
+};
 
 //* 유저닉네임 중복 검사
 const postDuplicateNickname = async (userId: number, nickname: string) => {
-    let isExisted = 1;
+  let isExisted = 1;
 
-    const lowercaseNickname: string = nickname.toLowerCase();
+  const lowercaseNickname: string = nickname.toLowerCase();
 
-    const userCheck = await prisma.user.findFirst({
-        where: {
-            nickname: {
-                equals: lowercaseNickname,
-                mode: "insensitive"
-            }
-        },
-        select: {
-            nickname: true
-        }
-    });
+  const userCheck = await prisma.user.findFirst({
+    where: {
+      nickname: {
+        equals: lowercaseNickname,
+        mode: "insensitive",
+      },
+    },
+    select: {
+      nickname: true,
+    },
+  });
 
-    if (!userCheck) {
-        isExisted = 0;
-    };
+  if (!userCheck) {
+    isExisted = 0;
+  }
 
-    const data = {
-        check: isExisted
-    }
-    return data;
-}
+  const data = {
+    check: isExisted,
+  };
+  return data;
+};
 
 //* refreshToken으로 유저 검색
 const getUserByRfToken = async (refreshToken: string) => {
-    const user = prisma.user.findFirst({
-        where: {
-            refresh_token: refreshToken
-        }
-    });
+  const user = prisma.user.findFirst({
+    where: {
+      refresh_token: refreshToken,
+    },
+  });
 
-    return user;
-}
+  return user;
+};
 
-const getUserVersion = async (userId: number) => {
-    const versionData = await prisma.version.findUnique({
-        where: {
-            id: 1
-        },
-        select: {
-            imageUrl: true,
-            iosForceVersion: true,
-            androidForceVersion: true,
-            text: true
-        }
-    });
+const getUserVersion = async (
+  userId: number
+): Promise<UserVersionDTO | null> => {
+  const versionData = await prisma.version.findUnique({
+    where: {
+      id: 1,
+    },
+    select: {
+      imageUrl: true,
+      iosForceVersion: true,
+      androidForceVersion: true,
+      text: true,
+    },
+  });
 
-    return versionData;
-}
+  if (!versionData) {
+    return null;
+  }
+
+  const userVersionDTO: UserVersionDTO = {
+    androidForceVersion: versionData.androidForceVersion || "",
+    imageUrl: versionData.imageUrl || "",
+    text: versionData.text || "",
+    iosForceVersion: versionData.iosForceVersion || "",
+  };
+
+  return userVersionDTO;
+};
 
 const userService = {
-    getUser,
-    getUserIntro,
-    postDuplicateNickname,
-    getUserByRfToken,
-    getUserVersion
+  getUser,
+  getUserIntro,
+  postDuplicateNickname,
+  getUserByRfToken,
+  getUserVersion,
 };
 
 export default userService;
