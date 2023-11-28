638625ca131a702a5fc3c8ec2c18e6ca431b544a
yeseul106 <20191037@sungshin.ac.kr>
2023-11-28 10:21:49 +0900

    [Fix] multer-s3 버전 호환 문제 해결

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,21 +6,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 const envFound = dotenv.config();
 
 if (envFound.error) {
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 export default {
-    port: parseInt(process.env.PORT as string, 10) as number,
+  port: parseInt(process.env.PORT as string, 10) as number,
 
-    //? 데이터베이스
-    database: process.env.DATABASE_URL as string,
+  //? 데이터베이스
+  database: process.env.DATABASE_URL as string,
 
-    //? AWS
-    s3AccessKey: process.env.S3_ACCESS_KEY as string,
-    s3SecretKey: process.env.S3_SECRET_KEY as string,
-    bucketName: process.env.S3_BUCKET as string,
+  //? AWS
+  s3AccessKey: process.env.S3_ACCESS_KEY as string,
+  s3SecretKey: process.env.S3_SECRET_KEY as string,
+  bucketName: process.env.S3_BUCKET as string,
 
-    //? Email
-    email: process.env.MANAGER_EMAIL as string,
-    password: process.env.MANAGER_PASSWORD as string,
+  //? Email
+  email: process.env.MANAGER_EMAIL as string,
+  password: process.env.MANAGER_PASSWORD as string,
 };
