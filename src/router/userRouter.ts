41edda966202bd5c5c00f3419d05137289c7c76b
yeseul106 <20191037@sungshin.ac.kr>
2023-12-08 15:25:03 +0900

    [feat] #234 강제 업데이트 버전 확인 시, 토큰 검증 로직 제거

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -5,8 +5,8 @@ import { auth } from "../middlewares";
 const router: Router = Router();
 
 //* 닉네임 중복 검사 POST /user/duplicate
-router.post("/duplicate", userController.postDuplicateNickname);
+router.post("/duplicate", auth, userController.postDuplicateNickname);
 //* 유저 업데이트 버전 확인하기 GET /user/version
-router.get("/v1/version", auth, userController.getUserVersion)
+router.get("/v1/version", userController.getUserVersion);
 
 export default router;
