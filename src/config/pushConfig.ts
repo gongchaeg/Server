47d62c4f14897066a207a0740aa8b62e755aa450
yeseul106 <68415644+yeseul106@users.noreply.github.com>
2024-05-17 02:24:00 +0900

    [Chore] firebase 관련 키값 파일 이름 수정

--- src/config/pushConfig.ts
+++ src/config/pushConfig.ts
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert(
-    require("../../new-peekabook-firebase-adminsdk-ke3ck-2c155d0a19.json")
+    require("../../service-account-file.json")
   ),
 });
 
